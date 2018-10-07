368fcd5f9fcf11e272fe205764ad98fc69908740
xlin0001 <xlin0003@student.monash.edu>
2018-10-07 20:33:46 +1100

    Initial Commit

---
+++ PMPSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PMPSystem
+//
+//  Created by Oliver Lin on 7/10/18.
+//  Copyright © 2018 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PMPSystemTests/PMPSystemTests.swift
@@ -0,0 +1,34 @@
+//
+//  PMPSystemTests.swift
+//  PMPSystemTests
+//
+//  Created by Oliver Lin on 7/10/18.
+//  Copyright © 2018 Monash University. All rights reserved.
+//
+
+import XCTest
+@testable import PMPSystem
+
+class PMPSystemTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
